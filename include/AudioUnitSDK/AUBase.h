c8b320f2192b5932defd6910562e58dcf355081f
reuk <reuk@users.noreply.github.com>
2023-12-07 14:38:21 +0000

    Fix out-of-bounds read in FillInParameterName

--- include/AudioUnitSDK/AUBase.h
+++ include/AudioUnitSDK/AUBase.h
@@ -510,7 +510,7 @@ protected:
 		if (inShouldRelease) {
 			ioInfo.flags |= kAudioUnitParameterFlag_CFNameRelease;
 		}
-		CFStringGetCString(inName, &ioInfo.name[0], offsetof(AudioUnitParameterInfo, clumpID),
+		CFStringGetCString(inName, std::data(ioInfo.name), std::size(ioInfo.name),
 			kCFStringEncodingUTF8);
 	}
 
